9d0ca6ea6f2acdafd7eb383ff8a758accc99f0df
ashutosh7761 <goluashu3@gmail.com>
2026-04-11 17:47:47 +0530

    AuthN AuthZ L7

---
+++ AuthN AuthZ L7/config/database.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose');
+
+require('dotenv').config();
+
+exports.connect = () =>{
+    mongoose.connect(process.env.DATABASE_URL)
+    .then(() => {console.log('Connected to MongoDB')})
+    .catch((err) => {
+        console.error('Error connecting to MongoDB', err);
+        process.exit(1);
+    });
+}
---
+++ AuthN AuthZ L7/controllers/auth.js
@@ -0,0 +1,44 @@
+const bcrypt = require('bcrypt');
+const User = require('../model/User');
+
+// signup route handler
+exports.signup = async (req , res) => {
+    try {
+        // get data 
+        const {name, email, password, role} = req.body;
+
+        //check if user already exists
+        const existingUser = await User.findOne({email});
+        if(existingUser){
+            return res.status(400).json({message: 'User already exists'});
+        }
+
+        // secure password
+        let hashedPassword;
+        try {
+            hashedPassword = await bcrypt.hash(password, 10);
+        }
+        catch (error) {
+            return res.status(500).json({
+                success: false,
+                message: 'Error hashing password',
+            })
+        }
+
+        // create entry for user 
+        const user = new User({
+            name, email , password: hashedPassword, role
+        })
+        
+        return res.status(200).json({
+            success: true,
+            message: 'User created successfully',
+        });
+    } catch (error) {
+        console.error('Error in signup:', error);
+        return res.status(500).json({
+            success: false,
+            message: ' User cannot be registered ',
+        });
+    }
+} 
