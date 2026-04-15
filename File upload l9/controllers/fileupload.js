5ab5ceac4f4b0b1bbf92e25bf635c92eb9796478
ashutosh7761 <goluashu3@gmail.com>
2026-04-16 00:53:51 +0530

    file upload l9

--- File upload l9/controllers/fileupload.js
+++ File upload l9/controllers/fileupload.js
@@ -3,8 +3,23 @@ const File = require('../models/file');
 // localfileupload ka handler function create karna hai 
 exports.localFileUpload = async (req, res) => {
     try {
-        
+       
+        // fetch file
+        const file = req.files.file;
+        console.log("lo jii file aagaya ",file);
+
+        let path = __dirname + "/files/" + Date.now();
+        console.log("lo jii path bhi aagaya ",path);
+
+        file.mv(path, (err) => {
+            console.log(err);
+        });
+
+        res.json({
+            success: true,
+            message: "local file uploaded successfully",
+        });
     } catch (error) {
-        
+        console.log(error);
     }
 }
