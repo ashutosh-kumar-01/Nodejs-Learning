4c0b70ae7c29be19298e25a798b5320bedc5a183
ashutosh7761 <goluashu3@gmail.com>
2026-04-16 16:33:45 +0530

    comments

--- File upload l9/controllers/fileupload.js
+++ File upload l9/controllers/fileupload.js
@@ -8,9 +8,12 @@ exports.localFileUpload = async (req, res) => {
         const file = req.files.file;
         console.log("lo jii file aagaya ",file);
 
+        // create path where file neeed to  store on server
         let path = __dirname + "/files/" + Date.now() + `.${file.name.split(".")[1]}`;
         console.log("lo jii path bhi aagaya ",path);
 
+
+        // add path to move function
         file.mv(path, (err) => {
             console.log(err);
         });
