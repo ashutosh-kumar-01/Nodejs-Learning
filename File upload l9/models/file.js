9450d73c0d10699c91b8f44d4778d82ad292ecbd
ashutosh7761 <goluashu3@gmail.com>
2026-04-19 17:23:31 +0530

    compleated node js learning now move towards project

--- File upload l9/models/file.js
+++ File upload l9/models/file.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const nodemailer = require('nodemailer');
 
 const fileSchema = new mongoose.Schema({
     name: {
@@ -16,6 +17,35 @@ const fileSchema = new mongoose.Schema({
         type: String,
     }
 });
+
+// post middleware 
+fileSchema.post('save', async function(doc) {
+    try {
+        console.log("DOC",doc);
+        // CREATE A TRANSPORTER
+        const transporter = nodemailer.createTransport({
+            host: process.env.MAIL_HOST,
+            auth:{
+                user: process.env.MAIL_USER,
+                pass: process.env.MAIL_PASS,
+            },
+        });
+
+        // send mail
+        let info = await transporter.sendMail({
+            from: `Gmoludon ke traf se hai ya mail`,
+            to: doc.email,
+            subject: "New file uploaded",
+            html: `<h2> Hello, Jii kaise hai aap log </h2>
+            <p> Aapka file successfully upload ho chuka hai, jiska naam hai ${doc.imageUrl} </p>`,
+        })
+        console.log("Mail sent", info);
+
+    } catch (error) {
+        console.error(error);
+
+    }
+})
 
 const File = mongoose.model('File', fileSchema);
 
