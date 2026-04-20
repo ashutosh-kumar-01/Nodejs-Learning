4de290fc6619b7b61ca1530f32c64576437360e3
ashutosh7761 <goluashu3@gmail.com>
2026-04-20 17:23:48 +0530

    studyapp ( created all models )

---
+++ studyapp/backend/config/database.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose');
+
+require('dotenv').config();
+
+exports.connect = () => {
+    mongoose.connect(process.env.MONGODB_URI)
+    .then(() => {
+        console.log('Connected to MongoDB');
+    })
+    .catch((err) => {
+        console.error('Error connecting to MongoDB:', err);
+        process.exit();
+    });
+};
