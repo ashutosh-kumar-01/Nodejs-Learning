9712dfc95b7fc21f8f25f765cb9532078339ae44
ashutosh7761 <goluashu3@gmail.com>
2026-04-22 21:25:58 +0530

    created login controller

--- studyapp/backend/controllers/Auth.js
+++ studyapp/backend/controllers/Auth.js
@@ -3,6 +3,8 @@ const User = require("../models/User");
 const OTP = require("../models/OTP");
 const otpGenerator = require("otp-generator");  
 const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+require("dotenv").config();
 
 // send OTP
 
@@ -181,7 +183,54 @@ exports.signup = async (req, res) => {
 // login
 exports.login = async (req , res ) => {
     try {
-        
+        // get data from request body 
+        const {email, password} = req.body;
+        // validation data 
+        if(!email || !password){
+            return res.status(400).json({
+                success: false,
+                message: "All fields are required, please try again",
+            });
+        }
+        // user check exist or not 
+        const user = await User.findOne({email}).populate("additionalDetails");
+        if(!user){
+            return res.status(400).json({
+                success: false,
+                message: "User not found with this email, please signup first",
+            });
+        }
+        // generate JWT token , after password match 
+        if(await bcrypt.compare(password, user.password)){
+            const payload = {
+                email: user.email,
+                id: user._id,
+                role:user.role,
+            }
+            const token = jwt.sign(payload, process.env.JWT_SECRET,{
+                expiresIn: "2h",
+            });
+            user.token = token;
+            user.password = undefined;
+
+            // create cookie and send response
+            const options = {
+                expires: new Date(Date.now() + 3*24*60*60*1000),
+                httpOnly: true,
+            }
+
+            res.cookie("token", token, options).status(200).json({
+                success: true,
+                message: "User logged in successfully",
+            })
+            
+            }
+            else{
+                return res.status(401).json({
+                    success: false,
+                    message:"Invalid credentials, please try again",
+                });
+    }
     } catch (error) {
         console.error(error);
         return res.status(500).json({
